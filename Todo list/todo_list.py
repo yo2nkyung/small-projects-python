16512f50201e5017493518081a1e5ec67bc21fc5
YOONKYUNG LEE <yklee.tess@gmail.com>
2024-06-19 17:58:01 +0900

    commit todo_list program
    Implemented 7 features
    1: Add a new to-do
    2: Show your current to-do list
    3: Mark a specific to-do as complete
    4: Delete a specific to-do
    5: Edit a specific to-do
    6: Search for a to-do
    7: Save & exit your to-do list

--- Todo list/todo_list.py
+++ Todo list/todo_list.py
@@ -1,11 +1,17 @@
 import json
 
+priority_map = {'low': 1, 'medium': 2, 'high': 3}
+
+def sort_todo_list(todo_list):
+    return sorted(todo_list, key = lambda x: priority_map[x["priority"]])
+
 def add_todo_item(todo_list):
     new_item = input("Enter a new item: ")
-    todo_list.append(new_item)
-    print(f"{new_item} has been added to your todo list.")
-
-    return todo_list
+    priority = input("Enter the priority: (low, medium, high): ").lower()
+    todo_list.append({"item": new_item, "completed": False, "priority": priority})
+    print(f"{new_item} with priority {priority} has been added to your todo list.")    
+    
+    return sort_todo_list(todo_list)
 
 def show_todo_list(todo_list):
     if not todo_list:
@@ -13,7 +19,25 @@ def show_todo_list(todo_list):
     else:
         print("\nYour todo list:")
         for i, item in enumerate(todo_list, 1):
-            print(f"{i}. {item}\n")
+            status = "✔️" if item["completed"] else "❌"
+            print(f"{i}. {item['item']} [priority: {item['priority']}] {status}\n")
+
+def mark_todo_as_completed(todo_list):
+    if not todo_list:
+        print("Your todo list is empty.")
+    else:
+        show_todo_list(todo_list)
+        try:
+            item_number = int(input("Choose a number to mark as completed: "))
+            if 1<= item_number <= len(todo_list):
+                todo_list[item_number - 1]["completed"] = True
+                print(f"{todo_list[item_number - 1]['item']} has been marked as completed.")
+            else:
+                print("Invalid item number.")
+        except ValueError:
+            print("Enter a valid number.")
+
+    return sort_todo_list(todo_list)
 
 def del_todo_item(todo_list):
     if not todo_list:
@@ -24,11 +48,30 @@ def del_todo_item(todo_list):
             del_item = int(input("Choose item number to remove: "))
             if 1 <= del_item <= len(todo_list):
                 removed_item = todo_list.pop(del_item - 1)
-                print(f"{removed_item} has been successfully deleted.")
+                print(f"{removed_item['item']} has been successfully deleted.")
             else:
                 print("Invalid item number.")
         except ValueError:
             print("Enter a valid number.")
+
+    return sort_todo_list(todo_list)
+
+def edit_todo_item(todo_list):
+    if not todo_list:
+        print("Your todo list is empty.")
+    else:
+        show_todo_list(todo_list)
+        try:
+            item = int(input("Enter the number of the item to edit: "))
+            if 1 <= item <= len(todo_list):
+                new_item = input("Enter the new description: ")
+                todo_list[item-1]['item'] = new_item
+                print(f"Item {item} has been updated to {new_item}.")
+            else:
+                print("Invalid item number.")
+        except:
+            print("Enter a valid number.")
+    return sort_todo_list(todo_list)
 
 def save_todo_list(todo_list, output_file):
     try:
@@ -56,6 +99,27 @@ def load_todo_list(file_path):
         
     return todo_list
 
+def search_todo_items(todo_list):
+    if not todo_list:
+        print("Your todo list is empty.")
+    else:
+        search_keyword = input("Enter keyword to search: ").lower()
+        '''
+        search_result = []        
+        for item in todo_list:
+            if search_keyword in item["item"]:
+                search_result.append(item)
+                '''
+        search_result = [item for item in todo_list if search_keyword in item["item"]]
+
+
+        if not search_result:
+            print("No result.")
+        else:
+            print("\nSearch result:")
+            for i, item in enumerate(search_result, 1):
+                status = "✔️" if item["completed"] else "❌"
+                print(f"{i}. {item['item']} [priority: {item['priority']}] {status}\n")
 
 
 file_path = 'todo_file.json'
@@ -65,18 +129,27 @@ while True:
     print("\nTodo List Menu:")
     print("1. Add a new todo item")
     print("2. Show todo list")
-    print("3. Delete a todo item")
-    print("4. Save and exit")
+    print("3. Mark as completed")
+    print("4. Delete a todo item")
+    print("5. Change item description")
+    print("6. Search item")
+    print("7. Save and exit")
 
     user_choice = input("Choose an option: ")
 
     if user_choice == '1':
-        add_todo_item(todo_list)
+        todo_list = add_todo_item(todo_list)
     elif user_choice == '2':
         show_todo_list(todo_list)
     elif user_choice == '3':
-        del_todo_item(todo_list)
+        todo_list = mark_todo_as_completed(todo_list)
     elif user_choice == '4':
+        todo_list = del_todo_item(todo_list)
+    elif user_choice == '5':
+        todo_list = edit_todo_item(todo_list)
+    elif user_choice == '6':
+        search_todo_items(todo_list)
+    elif user_choice == '7':
         save_todo_list(todo_list, file_path)
         break
     else:
