6bcc87fce5405ef820f92d6b70d16dfb0a3baec5
YOONKYUNG LEE <yklee.tess@gmail.com>
2024-06-28 18:50:38 +0900

    Create weather_info.py
    A program that allows you to search for weather by entering the name of a location and shows the current weather and a 5-day forecast.

---
+++ Weather Info/weather_info.py
@@ -0,0 +1,131 @@
+import json
+import requests
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import io
+
+def create_main_window():
+    window = tk.Tk()
+    window.title("Weather Application")
+
+    # Location input field
+    tk.Label(window, text="Enter location:").pack(pady=5)
+    location_entry = tk.Entry(window)
+    location_entry.pack(pady=5)
+
+    # weather info field
+    weather_info = tk.Label(window, text="_", justify="left")
+    weather_info.pack(pady=10)
+
+    weather_icon_label = tk.Label(window)
+    weather_icon_label.pack(pady=10)
+
+    forecast_info = tk.Label(window, text="=", justify="left")
+    forecast_info.pack(pady=10)
+
+    def get_weather():
+        location = location_entry.get()
+        if location:
+            weather_data = fetch_weather(location, api_key)
+            
+            if weather_data:
+                display_weather(weather_data, weather_info, weather_icon_label)
+                forecast_data = fetch_forecast(location, api_key)
+                if forecast_data:
+                    display_forecast(forecast_data, forecast_info)
+                else:
+                    messagebox.showerror("Error", "Could not retrieve forecast data.")
+            else:
+                messagebox.showerror("Error", "Could not retrieve weather data.")
+        else:
+            messagebox.showerror("Input Required", "Please enter a location.")
+    
+    fetch_button = tk.Button(window, text="Get Weather", command=get_weather)
+    fetch_button.pack(pady=5)
+
+    window.mainloop()
+
+def fetch_weather(location, api_key):
+    #API URL
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': location,
+        'appid': api_key,
+        'units': 'metric'
+    }
+
+    try:
+        result = requests.get(url, params=params)
+        result.raise_for_status()
+        weather_data = result.json()
+        with open('weather_data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(weather_data, json_file, ensure_ascii=False, indent=4)
+        return weather_data
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def fetch_forecast(location, api_key):
+    url = "https://api.openweathermap.org/data/2.5/forecast"
+    params = {
+        'q': location,
+        'appid': api_key,
+        'units': 'metric'
+    }
+
+    try:
+        result = requests.get(url, params=params)
+        result.raise_for_status()
+        forecast_data = result.json()
+        return forecast_data
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def display_weather(weather_data, weather_info, weather_icon_label):
+    try:
+        location = weather_data['name']
+        country = weather_data['sys']['country']
+        temperature = weather_data['main']['temp']
+        humidity = weather_data['main']['humidity']
+        desctription = weather_data['weather'][0]['description']
+        icon_num = weather_data['weather'][0]['icon']
+
+        weather_info.config(text=(
+            f"Weather in {location}, {country}\n"
+            f"Temperature: {temperature}°C\n"
+            f"Humidity: {humidity}%\n"
+            f"Description: {desctription.capitalize()}"
+        ))
+
+        # weather icon
+        icon_url = f"https://openweathermap.org/img/wn/{icon_num}@2x.png"
+        icon_result = requests.get(icon_url)
+        icon_image = Image.open(io.BytesIO(icon_result.content))
+        icon_photo = ImageTk.PhotoImage(icon_image)
+
+        weather_icon_label.config(image=icon_photo)
+        weather_icon_label.image = icon_photo # Prevent images from being removed by garbage collection.
+    except KeyError as e:
+        weather_info.config(text = f"Error extracting weather data: {e}")
+
+def display_forecast(forecast_data, forecast_info):
+    try:
+        forecast_text = "5-day Forecast\n"
+        for forecast in forecast_data["list"][:5]:
+            date = forecast["dt_txt"]
+            temp = forecast["main"]["temp"]
+            description = forecast["weather"][0]["description"]
+            forecast_text += f"{date} {temp}°C {description.capitalize()}\n"
+
+        forecast_info.config(text = forecast_text)
+    except KeyError as e:
+        forecast_info.config(text = f"Error extracting weather data: {e}")
+
+
+
+api_key = "cea480f592567920fdcda69d9aba4577" # Change to the API key issued by OpenWeatherMap.
+
+if __name__ == "__main__":
+    create_main_window()
